42033814203f800d1ac66504ce542e4268122f4e
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-06-09 22:51:04 +0530

    Add files via upload

---
+++ Ques 5.py
@@ -0,0 +1,5 @@
+numh = int(input("enter the number of hydrogen :"))
+numc = int(input("enter the number of carbon :"))
+numo = int(input("enter the number of oxygen "))
+molecular_weight = 1.00794*numh + 12.0107*numc + 15.9904*numo
+print("the molecular weight of molecule is",molecular_weight)
