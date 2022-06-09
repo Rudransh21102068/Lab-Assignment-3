20d8b830a88f63eed73f9afc17d82ccf52ede51b
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-06-09 22:50:50 +0530

    Add files via upload

---
+++ Ques 3.py
@@ -0,0 +1,20 @@
+import math
+
+print((3+4)*5)
+n = int(input("enter the number"))
+exp2 =  n*(1-1)/2
+print(exp2)
+r = int(input("enter the value of radius"))
+exp3 = 4*3.14*r
+print(exp3)
+a = float(input("enter the angle of cos"))
+b = float(input("enter the angle of sin"))
+exp4 = math.sqrt(r*math.cos(a)**2+r*math.sin(b)**2)
+print(exp4)
+y1 = float(input("enter the 1st y coordinte"))
+y2 = float(input("enter the 2nd y coordinate"))
+x1 = float(input("enter the 1st x coordinate"))
+x2 = float(input("enter the 2nd x coordinate"))
+exp5 = (y2-y1)/(x1-x2)
+print(exp5)
+5
