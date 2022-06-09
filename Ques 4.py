1488531d26560bf26158ee6dfe0dede3e21f5952
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-06-09 23:01:19 +0530

    Add files via upload

---
+++ Ques 4.py
@@ -0,0 +1,24 @@
+#Q4.a
+for i in range(5):
+    print(i, end = " ")
+print()
+
+#Q4.b
+for i in range(3,10):
+    print(i, end=" ")
+print()
+
+#Q4.c
+for i in range(4,13,3):
+    print(i, end=" ")
+print()
+
+#Q4.d
+for i in range(15,5,-2):
+    print(i, end=" ")
+print()
+
+#Q4.e
+for i in range(5,3):
+    print(i, end=" ")
+print(i)
